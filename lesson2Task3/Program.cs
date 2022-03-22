c37c823805b38f9fa9a5295d53241c182ee1160c
FlashMax <flashmax537@yandex.ru>
2022-03-22 19:25:16 +0400

    Выполнил практическое задание по уроку №2

--- lesson2Task3/Program.cs
+++ lesson2Task3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace lesson2Task3
 {
@@ -6,7 +7,105 @@ namespace lesson2Task3
     {
         static void Main()
         {
-            
+            var bankAccounts = new List<BankAccount>();
+
+            var bankAccount1 = new BankAccount();
+            bankAccounts.Add(bankAccount1);
+
+            var bankAccount2 = new BankAccount(balance: 100);
+            bankAccounts.Add(bankAccount2);
+
+            var bankAccount3 = new BankAccount(type: TypeOfBankAccount.DepositAccount);
+            bankAccounts.Add(bankAccount3);
+
+            var bankAccount4 = new BankAccount(200, TypeOfBankAccount.PaymentAccount);
+            bankAccounts.Add(bankAccount4);
+
+            foreach (var account in bankAccounts)
+            {
+                Console.WriteLine($"Номер счета: {account.GetNumber()}\n" +
+                    $"Баланс: {account.GetBalance()} у.е.\n" +
+                    $"Тип банковского счета: {account.GetTypeOfBankAccount()}\n");
+            }
         }
+    }
+
+    /// <summary>
+    /// Счет в банке
+    /// </summary>
+    public class BankAccount
+    {
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public BankAccount()
+        {
+            Number = _number++;
+            Balance = 0;
+            Type = TypeOfBankAccount.OtherAccount;
+        }
+
+        /// <summary>
+        /// Конструктор для заполнения поля баланс
+        /// </summary>
+        /// <param name="balance"></param>
+        public BankAccount(int balance)
+        {
+            Number = _number++;
+            Balance = balance;
+            Type = TypeOfBankAccount.OtherAccount;
+        }
+
+        /// <summary>
+        /// Конструктор для заполнения поля тип банковского счета
+        /// </summary>
+        /// <param name="type"></param>
+        public BankAccount(TypeOfBankAccount type)
+        {
+            Number = _number++;
+            Balance = 0;
+            Type = type;
+        }
+
+        /// <summary>
+        /// Конструктор для заполнения баланса и типа банковского счета
+        /// </summary>
+        /// <param name="balance"></param>
+        /// <param name="type"></param>
+        public BankAccount(int balance, TypeOfBankAccount type)
+        {
+            Number = _number++;
+            Balance = balance;
+            Type = type;
+        }
+
+        private static int _number = 1;
+        private int Number;
+        private int Balance;
+        private TypeOfBankAccount Type;
+
+        public string GetNumber()
+        {
+            return Number.ToString("D8"); ;
+        }
+
+        public int GetBalance()
+        {
+            return Balance;
+        }
+
+        public TypeOfBankAccount GetTypeOfBankAccount()
+        {
+            return Type;
+        }
+
+    }
+
+    public enum TypeOfBankAccount
+    {
+        DepositAccount = 1, // Депозитный счет
+        PaymentAccount = 2, // Расчетный счет
+        CurrencyAccount = 3, // Валютный счет
+        OtherAccount = 4 // Другое
     }
 }
--- lesson2Task4/Program.cs
+++ lesson2Task4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace lesson2Task4
 {
@@ -6,7 +7,48 @@ namespace lesson2Task4
     {
         static void Main()
         {
-            
+            var bankAccounts = new List<BankAccount>();
+
+            var bankAccount1 = new BankAccount();
+            bankAccount1.Balance = 100;
+            bankAccount1.Type = TypeOfBankAccount.DepositAccount;
+            bankAccounts.Add(bankAccount1);
+
+            var bankAccount2 = new BankAccount();
+            bankAccount2.Balance = 200;
+            bankAccount2.Type = TypeOfBankAccount.PaymentAccount;
+            bankAccounts.Add(bankAccount2);
+
+            var bankAccount3 = new BankAccount();
+            bankAccount3.Balance = 300;
+            bankAccount3.Type = TypeOfBankAccount.CurrencyAccount;
+            bankAccounts.Add(bankAccount3);
+
+            foreach (var account in bankAccounts)
+            {
+                Console.WriteLine($"Номер счета: {account.Number}\n" +
+                    $"Баланс: {account.Balance} у.е.\n" +
+                    $"Тип банковского счета: {account.Type}\n");
+            }
         }
+    }
+
+    /// <summary>
+    /// Счет в банке
+    /// </summary>
+    public class BankAccount
+    {
+        private static int _number = 1;
+        public string Number { get { return (_number++).ToString("D8"); ; } }
+
+        public int Balance { get; set; }
+        public TypeOfBankAccount Type { get; set; }
+    }
+
+    public enum TypeOfBankAccount
+    {
+        DepositAccount = 1, // Депозитный счет
+        PaymentAccount = 2, // Расчетный счет
+        CurrencyAccount = 3 // Валютный счет
     }
 }
