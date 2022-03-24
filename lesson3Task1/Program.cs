243c1aab03d8a431d322829d8ad75160348ab98a
FlashMax <flashmax537@yandex.ru>
2022-03-24 22:32:42 +0400

    Добавил проекты для урока №3

---
+++ lesson3Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace lesson3Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
---
+++ lesson3Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace lesson3Task3
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
