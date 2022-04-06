8bf074541f3279cd8bf5b25d88f744194a1f6208
FlashMax <flashmax537@yandex.ru>
2022-04-06 19:45:12 +0400

    Выполнил практическое задание по уроку №5

--- lesson5Task1/Program.cs
+++ lesson5Task1/Program.cs
@@ -6,6 +6,165 @@ namespace lesson5Task1
     {
         static void Main()
         {
+            var rationalNumberOne = new RationalNumbers(9, 2);
+            var rationalNumberTwo = new RationalNumbers(1, 3);
+            var duplicateRationalNumberOne = rationalNumberOne;
+
+            Console.WriteLine($"{rationalNumberOne.ToString()} + {rationalNumberTwo.ToString()} = {rationalNumberOne + rationalNumberTwo}");
+            Console.WriteLine($"{rationalNumberOne.ToString()} - {rationalNumberTwo.ToString()} = {rationalNumberOne - rationalNumberTwo}");
+            Console.WriteLine($"{rationalNumberOne.ToString()} * {rationalNumberTwo.ToString()} = {rationalNumberOne * rationalNumberTwo}");
+            Console.WriteLine($"{rationalNumberOne.ToString()} / {rationalNumberTwo.ToString()} = {rationalNumberOne / rationalNumberTwo}");
+            Console.WriteLine($"{rationalNumberOne.ToString()} == {rationalNumberTwo.ToString()} = {rationalNumberOne == rationalNumberTwo}");
+            Console.WriteLine($"{rationalNumberOne.ToString()} != {rationalNumberTwo.ToString()} = {rationalNumberOne != rationalNumberTwo}");
+            Console.WriteLine($"{rationalNumberOne.ToString()} > {rationalNumberTwo.ToString()} = {rationalNumberOne > rationalNumberTwo}");
+            Console.WriteLine($"{rationalNumberOne.ToString()} < {rationalNumberTwo.ToString()} = {rationalNumberOne < rationalNumberTwo}");
+            Console.WriteLine($"{rationalNumberOne.ToString()} >= {rationalNumberTwo.ToString()} = {rationalNumberOne >= rationalNumberTwo}");
+            Console.WriteLine($"{rationalNumberOne.ToString()} <= {rationalNumberTwo.ToString()} = {rationalNumberOne <= rationalNumberTwo}");
+            Console.WriteLine($"{rationalNumberOne.ToString()} % {rationalNumberTwo.ToString()} = {rationalNumberOne % rationalNumberTwo}");
+
+            Console.Write(rationalNumberOne.ToString());
+            rationalNumberOne++;
+            Console.WriteLine($"++ = {rationalNumberOne}");
+            Console.Write(rationalNumberOne.ToString());
+            rationalNumberOne--;
+            Console.WriteLine($"-- = {rationalNumberOne}");
+            Console.WriteLine();
+
+            Console.WriteLine($"Значение: {rationalNumberOne}; Тип: {rationalNumberOne.GetType().Name}");
+            Console.WriteLine($"Значение: {(float)rationalNumberOne}; Тип: {((float)rationalNumberOne).GetType().Name}");
+            Console.WriteLine($"Значение: {(int)rationalNumberOne}; Тип: {((int)rationalNumberOne).GetType().Name}");
+            Console.WriteLine();
+
+            Console.WriteLine($"({rationalNumberOne}).Equals({rationalNumberTwo}): {rationalNumberOne.Equals(rationalNumberTwo)}");
+            Console.WriteLine($"({rationalNumberOne}).Equals({duplicateRationalNumberOne}): {rationalNumberOne.Equals(duplicateRationalNumberOne)}");
+        }
+    }
+
+    public class RationalNumbers
+    {
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public RationalNumbers()
+        {
+
+        }
+
+        public RationalNumbers(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public int Numerator { get; set; }
+        public int Denominator { get; set; }
+
+        public static bool operator ==(RationalNumbers a, RationalNumbers b)
+        {
+            if ((a.Numerator / (decimal)a.Denominator) == (b.Numerator / (decimal)b.Denominator)) return true;
+            return false;
+        }
+        public static bool operator !=(RationalNumbers a, RationalNumbers b)
+        {
+            if ((a.Numerator / (decimal)a.Denominator) != (b.Numerator / (decimal)b.Denominator)) return true;
+            return false;
+        }
+
+        public static bool operator >(RationalNumbers a, RationalNumbers b)
+        {
+            if ((a.Numerator / (decimal)a.Denominator) > (b.Numerator / (decimal)b.Denominator)) return true;
+            return false;
+        }
+        public static bool operator <(RationalNumbers a, RationalNumbers b)
+        {
+            if ((a.Numerator / (decimal)a.Denominator) < (b.Numerator / (decimal)b.Denominator)) return true;
+            return false;
+        }
+
+        public static bool operator >=(RationalNumbers a, RationalNumbers b)
+        {
+            if ((a.Numerator / (decimal)a.Denominator) >= (b.Numerator / (decimal)b.Denominator)) return true;
+            return false;
+        }
+        public static bool operator <=(RationalNumbers a, RationalNumbers b)
+        {
+            if ((a.Numerator / (decimal)a.Denominator) <= (b.Numerator / (decimal)b.Denominator)) return true;
+            return false;
+        }
+
+        public static string operator +(RationalNumbers a, RationalNumbers b)
+        {
+            if (a.Denominator != b.Denominator)
+            {
+                return $"{(a.Numerator * b.Denominator) + (b.Numerator * a.Denominator)}/" +
+                    $"{(a.Denominator * b.Denominator)}";
+            }
+            else return $"{a.Numerator + b.Numerator}/{a.Denominator}";
+        }
+
+        public static string operator -(RationalNumbers a, RationalNumbers b)
+        {
+            if (a.Denominator != b.Denominator)
+            {
+                return new RationalNumbers((a.Numerator * b.Denominator) - (b.Numerator * a.Denominator), 
+                    (a.Denominator * b.Denominator)).ToString();
+            }
+            else return new RationalNumbers(a.Numerator - b.Numerator, a.Denominator).ToString();
+        }
+
+        public static string operator *(RationalNumbers a, RationalNumbers b)
+        {
+            return new RationalNumbers(a.Numerator * b.Numerator, a.Denominator * b.Denominator).ToString();
+        }
+
+        public static string operator /(RationalNumbers a, RationalNumbers b)
+        {
+            return new RationalNumbers(a.Numerator * b.Denominator, a.Denominator * b.Numerator).ToString();
+    }
+
+        public static RationalNumbers operator ++(RationalNumbers a)
+        {
+            return new RationalNumbers(a.Numerator + a.Denominator, a.Denominator);
+        }
+
+        public static RationalNumbers operator --(RationalNumbers a)
+        {
+            return new RationalNumbers(a.Numerator - a.Denominator, a.Denominator);
+        }
+
+        public static string operator %(RationalNumbers a, RationalNumbers b)
+        {
+            return new RationalNumbers((a.Numerator * b.Denominator) % (a.Denominator * b.Numerator), a.Denominator * b.Numerator).ToString();;
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+
+        public static explicit operator float(RationalNumbers a)
+        {
+            return (float)a.Numerator / a.Denominator;
+        }
+
+        public static explicit operator int(RationalNumbers a)
+        {
+            return a.Numerator / a.Denominator;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as RationalNumbers);
+        }
+
+        public bool Equals(RationalNumbers obj)
+        {
+            return obj.Numerator == this.Numerator && obj.Denominator == this.Denominator;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Numerator, Denominator);
         }
     }
 }
--- lesson5Task2/Program.cs
+++ lesson5Task2/Program.cs
@@ -6,6 +6,81 @@ namespace lesson5Task2
     {
         static void Main()
         {
+            var complexNumberOne = new ComplexNumbers(3, -2);
+            var complexNumberTwo = new ComplexNumbers(4, 1);
+            var complexNumberThree = new ComplexNumbers(4, 1);
+
+            var plusResult = complexNumberOne + complexNumberTwo;
+            var minusResult = complexNumberOne - complexNumberTwo;
+            var multiplyResult = complexNumberOne * complexNumberTwo;
+
+            Console.WriteLine($"({complexNumberOne.ToString()}) + ({complexNumberTwo.ToString()}) = {plusResult.ToString()}");
+            Console.WriteLine($"({complexNumberOne.ToString()}) - ({complexNumberTwo.ToString()}) = {minusResult.ToString()}");
+            Console.WriteLine($"({complexNumberOne.ToString()}) * ({complexNumberTwo.ToString()}) = {multiplyResult.ToString()}");
+            Console.WriteLine();
+            Console.WriteLine($"({complexNumberOne.ToString()}) == ({complexNumberThree.ToString()}): {complexNumberOne == complexNumberThree}");
+            Console.WriteLine($"({complexNumberTwo.ToString()}) == ({complexNumberThree.ToString()}): {complexNumberTwo == complexNumberThree}");
+            Console.WriteLine();
+            Console.WriteLine($"({complexNumberOne.ToString()}) != ({complexNumberThree.ToString()}): {complexNumberOne != complexNumberThree}");
+            Console.WriteLine($"({complexNumberTwo.ToString()}) != ({complexNumberThree.ToString()}): {complexNumberTwo != complexNumberThree}");
+        }
+    }
+
+    public class ComplexNumbers
+    {
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public ComplexNumbers()
+        {
+
+        }
+
+        public ComplexNumbers(int a, int b)
+        {
+            A = a;
+            B = b;
+        }
+
+        /// <summary>
+        /// Действительное число A
+        /// </summary>
+        public int A { get; set; }
+
+        /// <summary>
+        /// Действительное число B
+        /// </summary>
+        public int B { get; set; }
+
+        public override string ToString()
+        {
+            return B > 0 ? $"{A} + {B}i" : $"{A} - {Math.Abs(B)}i";
+        }
+
+        public static string operator +(ComplexNumbers numberOne, ComplexNumbers numberTwo)
+        {
+            return new ComplexNumbers(numberOne.A + numberTwo.A, numberOne.B + numberTwo.B).ToString();
+        }
+
+        public static string operator -(ComplexNumbers numberOne, ComplexNumbers numberTwo)
+        {
+            return new ComplexNumbers(numberOne.A - numberTwo.A, numberOne.B - numberTwo.B).ToString();
+        }
+
+        public static string operator *(ComplexNumbers numberOne, ComplexNumbers numberTwo)
+        {
+            return new ComplexNumbers((numberOne.A * numberTwo.A) + (numberOne.B * numberTwo.B * -1),
+                (numberOne.A * numberTwo.B) + (numberOne.B * numberTwo.A)).ToString();
+        }
+
+        public static bool operator ==(ComplexNumbers numberOne, ComplexNumbers numberTwo)
+        {
+            return numberOne.A == numberTwo.A && numberOne.B == numberTwo.B;
+        }
+
+        public static bool operator !=(ComplexNumbers numberOne, ComplexNumbers numberTwo)
+        {
+            return numberOne.A != numberTwo.A && numberOne.B != numberTwo.B;
         }
     }
 }
